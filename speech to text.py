d2f967925d18e29d600f23583143b860678b20aa
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-04 13:04:23 +0530

    Add files via upload

---
+++ speech to text.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+speak("to whom buddy")
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("speak something.......")             # plss speak after this statement printed in the output window 
+    r.adjust_for_ambient_noise(source)
+    audio = r.listen(source)
+    try:
+        query = r.recognize_google(audio)
+        print("user said : ",end=" ")
+        print(query)
+
+    except Exception as e:
+        print("say that again please")          # if it is not able to recognize ur voice means, this will print in the output window 
+
+
+speak("hello")
+speak(query)
